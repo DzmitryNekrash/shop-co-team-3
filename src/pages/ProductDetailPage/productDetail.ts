8933f634db039671c85df5701ff56b42f113a327
Dzmitry_Nekrash <Dzmitry.Nekrash@student.ehu.lt>
2025-02-05 20:40:39 +0200

    feat: add product image section

--- src/pages/ProductDetailPage/productDetail.ts
+++ src/pages/ProductDetailPage/productDetail.ts
@@ -1,22 +1,24 @@
-import { createHeader } from '../../components/header';
-// import { renderProductDetail } from '../../components/addToCart/addToCart';
+import { createHeader } from '../../components/heeder/header';
+import { renderProductDetail } from '../../components/addToCart/addToCart';
 import { fetchProduct } from '../../components/addToCart/addToCart';
-import { createFooter } from '../../components/footer';
+import { createFooter } from '../../components/footer/footer';
+import { createProductImagesSection } from '../../components/productImages/productImages';
+
 
 createHeader();
+const productImageSection = await createProductImagesSection();
+document.body.appendChild(productImageSection);
 
 // renderProductDetail();
-export async function renderProductDetail(productId: string): Promise<HTMLElement> {
-    const section = document.createElement('section');
-    section.classList.add('product-detail-page'); 
+// export async function renderProductDetail(productId: string): Promise<HTMLElement> {
+//     const section = document.createElement('section');
+//     section.classList.add('product-detail-page'); 
   
-    const product = await fetchProduct(productId);
-    section.appendChild(renderProduct(product)); 
-    console.log('Рендеринг продукта:', productId);
-    return section;
-  }
+//     const product = await fetchProduct(productId);
+//     section.appendChild(renderProduct(product)); 
+//     console.log('Рендеринг продукта:', productId);
+//     return section;
+//   }
   
-
-
 createFooter();
 
